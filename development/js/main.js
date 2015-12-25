396e0f96737fb7b8c26b255c5e01977119f6ef43
shainanigans <me@shainakoval.com>
2015-12-25 16:49:39 +1100

    feat: Add infoWindows

--- development/js/main.js
+++ development/js/main.js
@@ -4,60 +4,83 @@
 var map;
 
 // Marker Model
-var markers = [
-    {
-        title: 'Golden Lotus',
-        position: {lat: -33.89840, lng: 151.17817}
-    },
-    {
-        title: 'Gigi\'s Pizzeria',
-        position: {lat: -33.89925, lng: 151.17759}
-    },
-    {
-        title: 'Suzy Spoon\'s Vegetarian Butcher',
-        position: {lat: -33.89261, lng: 151.18687}
-    },
-    {
-        title: 'Gelato Blue',
-        position: {lat: -33.89734, lng: 151.17945}
-    },
-    {
-        title: 'Bliss & Chips',
-        position: {lat: -33.89492, lng: 151.18170}
-    },
-    {
-        title: 'Vina',
-        position: {lat: -33.89976, lng: 151.17767}
-    },
-    {
-        title: 'Lentil As Anything',
-        position: {lat: -33.89966, lng: 151.17764}
-    },
-    {
-        title: 'Basil Pizza & Pasta',
-        position: {lat: -33.89343, lng: 151.18405}
-    },
-    {
-        title: 'Newtown Pies',
-        position: {lat: -33.89633, lng: 151.17984}
-    },
-    {
-        title: 'Blossoming Lotus',
-        position: {lat: -33.89449, lng: 151.18281}
-    },
-    {
-        title: 'Green Gourmet',
-        position: {lat: -33.89312, lng: 151.18421}
-    },
-    {
-        title: 'Superfood Sushi',
-        position: {lat: -33.89259, lng: 151.18548}
-    }
-];
+var markerModel = {
+    currentMarker: null,
+    markers: [
+        {
+            title: 'Golden Lotus',
+            position: {lat: -33.89840, lng: 151.17817},
+            info: '<div>test</div>'
+        },
+        {
+            title: 'Gigi\'s Pizzeria',
+            position: {lat: -33.89925, lng: 151.17759},
+            info: '<div>test</div>'
+        },
+        {
+            title: 'Suzy Spoon\'s Vegetarian Butcher',
+            position: {lat: -33.89261, lng: 151.18687},
+            info: '<div>test</div>'
+        },
+        {
+            title: 'Gelato Blue',
+            position: {lat: -33.89734, lng: 151.17945},
+            info: '<div>test</div>'
+        },
+        {
+            title: 'Bliss & Chips',
+            position: {lat: -33.89492, lng: 151.18170},
+            info: '<div>test</div>'
+        },
+        {
+            title: 'Vina',
+            position: {lat: -33.89976, lng: 151.17767},
+            info: '<div>tesfdfdfdt</div>'
+        },
+        {
+            title: 'Lentil As Anything',
+            position: {lat: -33.89966, lng: 151.17764},
+            info: '<div>test</div>'
+        },
+        {
+            title: 'Basil Pizza & Pasta',
+            position: {lat: -33.89343, lng: 151.18405},
+            info: '<div>test</div>'
+        },
+        {
+            title: 'Newtown Pies',
+            position: {lat: -33.89633, lng: 151.17984},
+            info: '<div>test</div>'
+        },
+        {
+            title: 'Blossoming Lotus',
+            position: {lat: -33.89449, lng: 151.18281},
+            info: '<div>test</div>'
+        },
+        {
+            title: 'Green Gourmet',
+            position: {lat: -33.89312, lng: 151.18421},
+            info: '<div>test</div>'
+        },
+        {
+            title: 'Superfood Sushi',
+            position: {lat: -33.89259, lng: 151.18548},
+            info: '<div>test</div>'
+        }
+    ]
+};
 
 var mapViewModel = {
     init: function() {
         mapView.init();
+    },
+
+    getMarkers: function() {
+      return markerModel.markers;
+    },
+
+    getCurrentMarker: function() {
+        return markerModel.currentMarker;
     }
 };
 
@@ -68,17 +91,29 @@ var mapView = {
             zoom: 15
         });
 
-        // Create markers on map
+        // Get the markers
+        var markers = mapViewModel.getMarkers();
+
+        // Create infoWindow
+        var infowindow = new google.maps.InfoWindow();
+
+        // Create markers on the page and attach infowindow
         for (i = 0; i < markers.length; i++) {
-            new google.maps.Marker({
+            var marker = new google.maps.Marker({
                 position: markers[i].position,
                 title: markers[i].title,
+                info: markers[i].info,
                 map: map
             });
+
+            google.maps.event.addListener(marker, 'click', function(e) {
+                infowindow.setContent(this.info);
+                infowindow.open(map, this);
+            });
         }
+
     }
 };
-
 
 /**********************
  *START APP
