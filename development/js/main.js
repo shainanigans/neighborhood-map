6174dd760240b210bce3305501d0bf599e3ee0da
shainanigans <me@shainakoval.com>
2015-12-25 14:01:32 +1100

    feat: Add two map markers

--- development/js/main.js
+++ development/js/main.js
@@ -2,11 +2,32 @@
  *MAP
  **********************/
 var map;
+
+var markers = [
+    {
+        title: 'Golden Lotus',
+        position: {lat: -33.89840, lng: 151.17817}
+    },
+    {
+        title: 'Gigi\'s Pizzeria',
+        position: {lat: -33.89925, lng: 151.17759}
+    }
+];
+
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -33.8999386, lng: 151.1714448},
-    zoom: 15
-  });
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -33.8999386, lng: 151.1714448},
+        zoom: 15
+    });
+
+    for (i = 0; i < markers.length; i++) {
+        new google.maps.Marker({
+            position: markers[i].position,
+            title: markers[i].title,
+            map: map
+        });
+    }
+
 };
 
 /**********************
